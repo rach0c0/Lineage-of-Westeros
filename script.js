ce7b152c395540718626e40e1a3811c799f3afdb
Rachel O <roconnellvisualartist@gmail.com>
2023-11-11 15:20:27 -0500

    added Lannister house

---
+++ script.js
@@ -0,0 +1,7 @@
+//  Stark house info box expantions
+document.querySelector('.Ned').addEventListener("click", () => {
+    document.querySelector(".family-memeber").classList.add("Expand")
+});
+document.querySelector('.btn-m').addEventListener("click", () => {
+    document.querySelector(".toggle-m-container").classList.toggle("Mance-open")
+});
